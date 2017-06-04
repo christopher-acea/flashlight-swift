68539f27a6bf6f4e71a3dd95aea58073ef286ca1
christopher-acea <chris@chriss-MacBook-Pro.local>
2017-06-04 12:11:55 -0400

    My first Flashlight app written in Swift

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -9,17 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       updateUI()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var lightOn = true
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        updateUI()
     }
-
-
-}
-
+    func updateUI(){
+            view.backgroundColor = lightOn ? .white : .black
+        }
+    }
